0f626aaa197abe53961037e79fbfe76b6964b46a
Alexandre Corso <alexandre@acorso.fr>
2020-04-13 20:06:33 +0200

    Fix / issue

--- app/modules/bird.py
+++ app/modules/bird.py
@@ -7,6 +7,29 @@ from aclook import appSC
 api = Namespace('bird', description="Bird endpoints")
 
 
+def convert_slash(network):
+    """
+    Use %252F on the uri -> replace %2F into /
+    :param network: 1.1.1.0%2F24
+    :return: 1.1.1.0/24
+    """
+    if "%2F" in network:
+        return network.replace("%2F", "/")
+    return network
+
+
+def merge_network_and_mask(network, mask):
+    """
+    Merge network and mask
+    :param network: 1.1.1.0
+    :param mask: 24
+    :return: 1.1.1.0/24
+    """
+    if mask:
+        return f"{network}/{mask}"
+    return network
+
+
 @api.route('/asn/<asn>')
 class BirdRouteFromASN(Resource):
 
@@ -19,6 +42,7 @@ class BirdRouteFromASN(Resource):
         return appSC.bird_cmd().get_route_origin_asn(asn)
 
 
+@api.route('/network/<network>/<int:mask>')
 @api.route('/network/<network>')
 class BirdRoute(Resource):
 
@@ -26,11 +50,14 @@ class BirdRoute(Resource):
         responses={200: 'OK', 500: 'Timeout or internal error'},
         params={'network': 'Lookup IP or Network'}
     )
-    # noinspection PyMethodMayBeStatic
-    def get(self, network):
+    # noinspection PyMethodMayBeStatisc
+    def get(self, network, mask):
+        network = convert_slash(network)
+        network = merge_network_and_mask(network, mask)
         return appSC.bird_cmd().get_route(network)
 
 
+@api.route('/network/<network>/<int:mask>/detail')
 @api.route('/network/<network>/detail')
 class BirdRouteDetail(Resource):
 
@@ -39,10 +66,13 @@ class BirdRouteDetail(Resource):
         responses={200: 'OK', 500: 'Timeout or internal error'},
         params={'network': 'Lookup IP or Network'}
     )
-    def get(self, network):
+    def get(self, network, mask):
+        network = convert_slash(network)
+        network = merge_network_and_mask(network, mask)
         return appSC.bird_cmd().get_route(network, detail=True)
 
 
+@api.route('/network/<network>/<int:mask>/protocol/<protocol>')
 @api.route('/network/<network>/protocol/<protocol>')
 class BirdRouteAndProtocol(Resource):
 
@@ -54,11 +84,13 @@ class BirdRouteAndProtocol(Resource):
             'protocol': 'Protocol (as peer) you want to focus'
         }
     )
-    def get(self, network, protocol):
+    def get(self, network, mask, protocol):
+        network = convert_slash(network)
+        network = merge_network_and_mask(network, mask)
         return appSC.bird_cmd().get_route_from_protocol(network, protocol)
 
 
-@api.route('/network/<network>/protocol/<protocol>/detail')
+@api.route('/network/<network>/<int:mask>/protocol/<protocol>/detail')
 class BirdRouteAndProtocolDetail(Resource):
 
     # noinspection PyMethodMayBeStatic
@@ -69,5 +101,7 @@ class BirdRouteAndProtocolDetail(Resource):
             'protocol': 'Protocol (as peer) you want to focus'
         }
     )
-    def get(self, network, protocol):
+    def get(self, network, mask, protocol):
+        network = convert_slash(network)
+        network = merge_network_and_mask(network, mask)
         return appSC.bird_cmd().get_route_from_protocol(network, protocol, detail=True)

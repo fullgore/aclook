af27e5aff45719420cc4120b98ba3566b8668349
Alexandre Corso <alexandre@acorso.fr>
2020-04-14 11:48:45 +0200

    Test nested routes

--- app/tests/test_nested_routes.py
+++ app/tests/test_nested_routes.py
@@ -9,7 +9,9 @@ class TestNestedRoute(unittest.TestCase):
     def test_valid_route(self):
         assert ACBirdParser.parse_nested_route_list(route_a) == result_route_a
         assert ACBirdParser.parse_nested_route_list(route_b) == result_route_b
-
-    def test_unknown_route(self):
         assert ACBirdParser.parse_nested_route_list(route_c) == result_route_c
         assert ACBirdParser.parse_nested_route_list(route_d) == result_route_d
+
+    def test_unknown_route(self):
+        assert ACBirdParser.parse_nested_route_list(route_e) == {'routes': []}
+        assert ACBirdParser.parse_nested_route_list(route_f) == {'routes': []}
